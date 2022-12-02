daf8e7c908e6530db52b3563cdf011a11a313996
Dave Megins-Nicholas <gcndavidmn@gmail.com>
2022-12-02 11:07:18 +0000

    Day 2 part 2

--- day02/day02.go
+++ day02/day02.go
@@ -10,61 +10,83 @@ type Result int64
 
 const (
 	Win  Result = 0
-	Draw        = 1
-	Lose        = 2
+	Draw Result = 1
+	Lose Result = 2
 )
 
 func CalculateScore(path string) int {
-	scoreTable := map[Result]int{
-		Win:  6,
-		Draw: 3,
-		Lose: 0,
-	}
-
-	myDecryptionTable := map[string]string{
-		"X": "rock",
-		"Y": "paper",
-		"Z": "scissors",
-	}
-
-	opponentDecryptionTable := map[string]string{
-		"A": "rock",
-		"B": "paper",
-		"C": "scissors",
-	}
-
-	choiceScores := map[string]int{
-		"rock":     1,
-		"paper":    2,
-		"scissors": 3,
-	}
-
 	score := 0
 
 	lines := utils.ReadFile(path)
 
 	for i := 0; i < len(lines); i++ {
-		line := lines[i]
-		splitLine := strings.Split(line, " ")
-
-		opponentsChoice := opponentDecryptionTable[splitLine[0]]
-		myChoice := myDecryptionTable[splitLine[1]]
-
-		result := gameResult(myChoice, opponentsChoice)
-
-		score += choiceScores[myChoice]
-		score += scoreTable[result]
+		score += calculateRoundScore(lines[i])
 	}
 
 	return score
 }
 
-func gameResult(myChoice string, opponentChoice string) Result {
-	rules := map[string]string{
-		"rock":     "scissors",
-		"paper":    "rock",
-		"scissors": "paper",
+func CalculateScorePart2(path string) int {
+	score := 0
+
+	lines := utils.ReadFile(path)
+
+	for i := 0; i < len(lines); i++ {
+		score += calculateRoundScorePart2(lines[i])
 	}
+
+	return score
+}
+
+func calculateRoundScore(line string) int {
+	scoreTable := getScoreTable()
+	myDecryptionTable := getMyDecryptionTable()
+	opponentDecryptionTable := getOpponentDecryptionTable()
+	choiceScores := getChoiceScores()
+
+	splitLine := strings.Split(line, " ")
+
+	opponentsChoice := opponentDecryptionTable[splitLine[0]]
+	myChoice := myDecryptionTable[splitLine[1]]
+
+	result := gameResult(myChoice, opponentsChoice)
+
+	return choiceScores[myChoice] + scoreTable[result]
+}
+
+func calculateRoundScorePart2(line string) int {
+	scoreTable := getScoreTable()
+	opponentDecryptionTable := getOpponentDecryptionTable()
+	choiceScores := getChoiceScores()
+	roundTargetResultTable := getRoundTargetResult()
+
+	splitLine := strings.Split(line, " ")
+
+	opponentsChoice := opponentDecryptionTable[splitLine[0]]
+	roundTarget := roundTargetResultTable[splitLine[1]]
+
+	myChoice := calculateTargetChoice(roundTarget, opponentsChoice)
+
+	result := gameResult(myChoice, opponentsChoice)
+
+	return choiceScores[myChoice] + scoreTable[result]
+}
+
+func calculateTargetChoice(target Result, opponentsChoice string) string {
+	rules := getRules()
+
+	switch target {
+	case Draw:
+		return opponentsChoice
+	case Lose:
+		return rules[opponentsChoice]
+	default:
+		return rules[rules[opponentsChoice]]
+	}
+}
+
+func gameResult(myChoice string, opponentChoice string) Result {
+	rules := getRules()
 
 	if myChoice == opponentChoice {
 		return Draw
@@ -74,5 +96,53 @@ func gameResult(myChoice string, opponentChoice string) Result {
 		return Win
 	} else {
 		return Lose
+	}
+}
+
+func getScoreTable() map[Result]int {
+	return map[Result]int{
+		Win:  6,
+		Draw: 3,
+		Lose: 0,
+	}
+}
+
+func getMyDecryptionTable() map[string]string {
+	return map[string]string{
+		"X": "rock",
+		"Y": "paper",
+		"Z": "scissors",
+	}
+}
+
+func getOpponentDecryptionTable() map[string]string {
+	return map[string]string{
+		"A": "rock",
+		"B": "paper",
+		"C": "scissors",
+	}
+}
+
+func getChoiceScores() map[string]int {
+	return map[string]int{
+		"rock":     1,
+		"paper":    2,
+		"scissors": 3,
+	}
+}
+
+func getRoundTargetResult() map[string]Result {
+	return map[string]Result{
+		"Z": Win,
+		"Y": Draw,
+		"X": Lose,
+	}
+}
+
+func getRules() map[string]string {
+	return map[string]string{
+		"rock":     "scissors",
+		"paper":    "rock",
+		"scissors": "paper",
 	}
 }
--- day02/day02_test.go
+++ day02/day02_test.go
@@ -6,12 +6,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestDay2(t *testing.T) {
+func TestDay2Part1(t *testing.T) {
 	score := CalculateScore("../inputs/day2.txt")
 	assert.Equal(t, 12586, score)
 }
 
-func TestDay2Example(t *testing.T) {
+func TestDay2Part1Example(t *testing.T) {
 	score := CalculateScore("../inputs/day2example.txt")
 	assert.Equal(t, 15, score)
+}
+
+func TestDay2Part2(t *testing.T) {
+	score := CalculateScorePart2("../inputs/day2.txt")
+	assert.Equal(t, 13193, score)
+}
+
+func TestDay2Part2Example(t *testing.T) {
+	score := CalculateScorePart2("../inputs/day2example.txt")
+	assert.Equal(t, 12, score)
 }

007efed41bf4eb88e9294828cef31f7c96ca3dbf
Dave Megins-Nicholas <gcndavidmn@gmail.com>
2022-12-04 14:29:07 +0000

    Day 4 part 2

--- day04/day04.go
+++ day04/day04.go
@@ -13,7 +13,7 @@ func SolvePart1(path string) int {
 	totalOverlaps := 0
 
 	for i := 0; i < len(lines); i++ {
-		if processLine(lines[i]) {
+		if processLinePart1(lines[i]) {
 			totalOverlaps++
 		}
 	}
@@ -21,7 +21,21 @@ func SolvePart1(path string) int {
 	return totalOverlaps
 }
 
-func processLine(line string) bool {
+func SolvePart2(path string) int {
+	lines := utils.ReadFile(path)
+
+	totalOverlaps := 0
+
+	for i := 0; i < len(lines); i++ {
+		if processLinePart2(lines[i]) {
+			totalOverlaps++
+		}
+	}
+
+	return totalOverlaps
+}
+
+func processLinePart1(line string) bool {
 	shifts := strings.Split(line, ",")
 
 	shiftAStart, shiftAEnd := parseShift(shifts[0])
@@ -33,6 +47,20 @@ func processLine(line string) bool {
 	intersection := utils.IntersectionInts(shiftARange, shiftBRange)
 
 	return len(intersection) == len(shiftARange) || len(intersection) == len(shiftBRange)
+}
+
+func processLinePart2(line string) bool {
+	shifts := strings.Split(line, ",")
+
+	shiftAStart, shiftAEnd := parseShift(shifts[0])
+	shiftBStart, shiftBEnd := parseShift(shifts[1])
+
+	shiftARange := utils.GenerateRange(shiftAStart, shiftAEnd)
+	shiftBRange := utils.GenerateRange(shiftBStart, shiftBEnd)
+
+	intersection := utils.IntersectionInts(shiftARange, shiftBRange)
+
+	return len(intersection) > 0
 }
 
 func parseShift(shift string) (int, int) {
--- day04/day04_test.go
+++ day04/day04_test.go
@@ -15,3 +15,13 @@ func TestDay4Part1(t *testing.T) {
 	score := SolvePart1("../inputs/day4.txt")
 	assert.Equal(t, 657, score)
 }
+
+func TestDay4Part2Example(t *testing.T) {
+	score := SolvePart2("../inputs/day4example.txt")
+	assert.Equal(t, 4, score)
+}
+
+func TestDay4Part2(t *testing.T) {
+	score := SolvePart2("../inputs/day4.txt")
+	assert.Equal(t, 938, score)
+}
